e9ba4004ec869c3034ff0160b8d8b87a0379ab86
LeraLonde <tys10062634@gmail.com>
2014-09-22 02:40:34 +0800

    Resolving the problem with the UI not displaying properly when running - tys

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,28 +3,19 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
+
 
 public class Main extends Application {
-	@SuppressWarnings("static-access")
-    @Override
+	@Override
 	public void start(Stage primaryStage) {
 		try {
-			displayUI(primaryStage);
+			UiComponent uiComponent = new UiComponent();
+			primaryStage.setScene(uiComponent.getScene());
+			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-	}
-	
-	public static void displayUI(Stage primaryStage) {
-		UiComponent mainUI = new UiComponent();
-		primaryStage.setScene(mainUI.getScene());
-		primaryStage.setTitle("WAVE WAVE!");
-		primaryStage.show();
-		mainUI.focusCommandInputBox();
 	}
 	
 	public static void main(String[] args) {
