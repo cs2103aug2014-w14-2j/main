10737bf2a46e794ac41ab321f51771d5fd9734f5
Xiao Jinyu <xjy_333@hotmail.com>
2014-10-07 13:00:59 +0800

    command class written, integrated with parser

--- src/application/Parser.java
+++ src/application/Parser.java
@@ -2,42 +2,60 @@ package application;
 
 /** This class implements a Parser to parse an input string
  * 
- * @author jinyu
- * @version 1.0
+ * @author Jinyu
+ * @version 2.0
  */
 
 public class Parser {
-	private String command_type;
-	private String task_name;
-	
-	/**
-	 * This constructs a parser object with an user input 
-	 * @param user_input   the one line command statement the user inputs
-	 */
-	
-	Parser(String user_input){
-		command_type = user_input.trim().split("\\s+")[0];
-		task_name =user_input.replace(getFirstWord(user_input), "").trim();
-	}
-	
-	/**
-	 * This returns the command type to be executed 
-	 * @return command type
-	 */
-	public String getCommand(){
-		return command_type;
-	}
-    
-	/**
-	 * This returns the task ID 
-	 * @return task ID
-	 */
-	public String getTaskName(){
-		return task_name;
-	}
-	
-	private static String getFirstWord(String input) {
-		String command = input.trim().split("\\s+")[0];
-		return command;
-	}
+
+
+    private Command cmd;
+    private String commandType;
+    private String taskID;
+    private String taskDesc;
+    private String taskTime;
+
+    /**
+     * This constructs a parser object with an user input 
+     * @param user_input   the one line command statement the user inputs
+     */
+
+    Parser(String userInput){
+        commandType = getFirstWord(userInput).toLowerCase();
+        switch (commandType) {
+        case "undo" : {
+            cmd = new Command(commandType);
+            break;
+        }
+        case "delete" : case "complete" : case "search" : 
+        {
+            taskID =userInput.replace(getFirstWord(userInput), "").trim();
+            cmd = new Command(commandType,taskID);
+            break;
+        }
+        case "add" : {
+            taskDesc = userInput.replace(getFirstWord(userInput), "").trim();
+            taskTime = "dummy";
+            cmd = new Command(commandType,taskDesc,taskTime);
+            break;
+        }
+        case "edit" : {
+            taskID = userInput.trim().split("\\s+")[1];
+            taskDesc = userInput.replace(getFirstWord(userInput)+" "+taskID,"").trim();
+            taskTime = null;
+            cmd = new Command(commandType, taskID, taskDesc, taskTime);
+            break;	    
+        }
+        default: 
+            break;
+        }
+    }
+
+    private String getFirstWord(String input) {
+        String command = input.trim().split("\\s+")[0];
+        return command;
+    }
+    public Command getCmd() {
+        return cmd;
+    }
 }
