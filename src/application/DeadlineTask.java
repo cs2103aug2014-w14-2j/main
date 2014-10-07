f4454a80af7abf01bbfa25d47b42831d9d04a83b
Sun Wang Jun <sunxperous@gmail.com>
2014-10-07 17:12:47 +0800

    add date attribute to deadline class

--- src/application/DeadlineTask.java
+++ src/application/DeadlineTask.java
@@ -1,9 +1,21 @@
 package application;
 
-public class DeadlineTask extends Task { 
+import java.util.Date;
+
+public class DeadlineTask extends Task {
+    
+    private Date deadline;
 
     public DeadlineTask() {
         // TODO Auto-generated constructor stub
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
     }
 
 }
