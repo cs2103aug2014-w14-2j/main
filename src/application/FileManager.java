9a0fabf7657dda4935f7a972729663665021744c
Kim Hyung Jon <a0115864@nus.edu.sg>
2014-10-07 22:32:33 +0800

    added methods for converting jsonarray <-> arraylist

--- src/application/FileManager.java
+++ src/application/FileManager.java
@@ -10,6 +10,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import java.util.Date;
 
 public class FileManager {
 	
@@ -21,11 +22,21 @@ public class FileManager {
 	private JSONArray deadlineTasks = new JSONArray();
 	private JSONArray timedTasks = new JSONArray();
 	
-	public void initiateFile(String filename) {
-		File file = new File(filename);
+	public FileManager() {
+		initiateFile();
+	}
+	
+	public void initiateFile() {
+		initiateFloatingFile();
+		initiateDeadlineFile();
+		initiateTimedFile();
+	}
+	
+	public void initiateFloatingFile() {
+		File file = new File(floatingFilename);
 		try {
 			if(!file.exists()) {
-				FileWriter createFile = new FileWriter(filename);
+				FileWriter createFile = new FileWriter(floatingFilename);
 				createFile.close();
 			}
 		} catch (IOException e) {
@@ -33,10 +44,42 @@ public class FileManager {
 		}
 	}
 	
-	public void retrieveLists() {
-		retrieveFloating();
-		retrieveDeadline();
-		retrieveTimed();
+	public void initiateDeadlineFile() {
+		File file = new File(deadlineFilename);
+		try {
+			if(!file.exists()) {
+				FileWriter createFile = new FileWriter(deadlineFilename);
+				createFile.close();
+			}
+		} catch (IOException e) {
+			
+		}
+		
+	}
+	
+	public void initiateTimedFile() {
+		File file = new File(timedFilename);
+		try {
+			if(!file.exists()) {
+				FileWriter createFile = new FileWriter(timedFilename);
+				createFile.close();
+			}
+		} catch (IOException e) {
+			
+		}
+	}
+	
+	public void retrieveLists(int type) {
+		if (type == 1) {
+			retrieveFloating();
+		} else if (type == 2) {
+			retrieveDeadline();
+		} else if (type == 3) {
+			retrieveTimed();
+		} else {
+			
+		}
+		
 	}
 	
 	public void retrieveFloating() {
@@ -77,10 +120,17 @@ public class FileManager {
 		
 	}
 	
-	public void saveToFiles() {
-		saveFloatingToFiles();
-		saveDeadlineToFiles();
-		saveTimedToFiles();
+	public void saveToFiles(int type) {
+		if (type == 1) {
+			saveFloatingToFiles();
+		} else if (type == 2) {
+			saveDeadlineToFiles();
+		} else if (type == 3) {
+			saveTimedToFiles();
+		} else {
+			
+		}
+		
 	}
 	
 	public void saveFloatingToFiles() {
@@ -118,35 +168,66 @@ public class FileManager {
 		
 	}
 	
-	public void buildArrayLists() {
-		buildFloatingArrayList();
-		buildDeadlineArrayList();
-		buildTimedArrayList();
-		
-	}
-	
-	public void buildFloatingArrayList() {
-		ArrayList<String> floatingArrayList = new ArrayList<String>();
-		for (int i = 0; i < floatingTasks.size(); i++) {
-			JSONObject obj = (JSONObject) floatingTasks.get(i);
+	public void convertFloatingArrayListToJSONArray(ArrayList<FloatingTask> list) {
+		floatingTasks.clear();
+		for (int i = 0; i < list.size(); i++) {
+			JSONObject obj = new JSONObject();
+			obj.put("ID", list.get(i).getId());
+			obj.put("Description", list.get(i).getDescription());
+			floatingTasks.add(obj);
 		}
 	}
 	
-	public void buildDeadlineArrayList() {
-		
+	public void convertDeadlineArrayListToJSONArray(ArrayList<DeadlineTask> list) {
+		deadlineTasks.clear();
+		for (int i = 0; i < list.size(); i++) {
+			JSONObject obj = new JSONObject();
+			obj.put("ID", list.get(i).getId());
+			obj.put("Description", list.get(i).getDescription());
+			obj.put("Deadline", list.get(i).getDeadline());
+			deadlineTasks.add(obj);
+		}
 	}
 	
-	public void buildTimedArrayList() {
-		
+	public void convertTimedArrayListToJSONArray(ArrayList<TimedTask> list) {
+		timedTasks.clear();
+		for (int i = 0; i < list.size(); i++) {
+			JSONObject obj = new JSONObject();
+			obj.put("ID", list.get(i).getId());
+			obj.put("Description", list.get(i).getDescription());
+			timedTasks.add(obj);
+		}
 	}
 	
-	public void convertToJSON() {
-		
+	public ArrayList<FloatingTask> convertFloatingJSONArrayToArrayList() {
+		ArrayList<FloatingTask> list = new ArrayList<FloatingTask>();
+		for (int i = 0; i < floatingTasks.size(); i++) {
+			FloatingTask task = new FloatingTask();
+			JSONObject obj = (JSONObject) floatingTasks.get(i);
+			task.setDescription((String) obj.get("Description")); 
+		}
+		return list;
 	}
-
-	public static void main(String[] args) {
-		
-
+	
+	public ArrayList<DeadlineTask> convertDeadlineJSONArrayToArrayList() {
+		ArrayList<DeadlineTask> list = new ArrayList<DeadlineTask>();
+		for (int i = 0; i < deadlineTasks.size(); i++) {
+			DeadlineTask task = new DeadlineTask();
+			JSONObject obj = (JSONObject) deadlineTasks.get(i);
+			task.setDescription((String) obj.get("Description")); 
+			task.setDeadline((Date) obj.get("Deadline"));
+		}
+		return list;
+	}
+	
+	public ArrayList<TimedTask> convertTimedJSONArrayToArrayList() {
+		ArrayList<TimedTask> list = new ArrayList<TimedTask>();
+		for (int i = 0; i < timedTasks.size(); i++) {
+			TimedTask task = new TimedTask();
+			JSONObject obj = (JSONObject) timedTasks.get(i);
+			task.setDescription((String) obj.get("Description")); 
+		}
+		return list;
 	}
 
 }
