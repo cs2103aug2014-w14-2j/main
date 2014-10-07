ef68b6d4e1a3aa24537304b3d297c6ee34ce66de
Xiao Jinyu <xjy_333@hotmail.com>
2014-10-07 15:14:32 +0800

    deleted lib folder

--- src/application/Time.java
+++ src/application/Time.java
@@ -9,8 +9,11 @@ public class Time
 {
    public static void main(String[] args)
    {
-      List<Date> dates = new PrettyTimeParser().parse("watch movie right now!");
+      List<Date> dates = new PrettyTimeParser().parse("watch movie!");
       System.out.println(dates.toString());
+      if (dates.toString().equals("[]")) {
+          System.out.println("testing successfully");
+      }
       // e.g. Prints: "[Sun Dec 12 13:45:12 CET 2013]"
    }
    
