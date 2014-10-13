22e76cebc29168033b14501e8c6ae13329d9dea6
Sun Wang Jun <sunxperous@gmail.com>
2014-10-14 01:41:01 +0800

    move ui initialization out of controller

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,14 +1,11 @@
 package application;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
 /**
  * The controller logic that integrates UI, Storage and Parser.
  * 
  * @author Sun Wang Jun
  */
-public class Controller extends Application {
+public class Controller {
 
     private static FileManager fileManage;
     
@@ -47,29 +44,12 @@ public class Controller extends Application {
         fileManage.saveToFiles();
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-            showStage(primaryStage);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void showStage(Stage primaryStage) {
-        uiComponent = new UiComponent();
-        primaryStage.setScene(uiComponent.getScene());
-        primaryStage.setResizable(false);
-        primaryStage.setTitle("WaveWave[0.2]");
-        primaryStage.show();
-    }
-
     public static void main(String[] args) {
         fileManage = new FileManager();
         fileManage.initiateFile();
         
         taskManager = new TaskManager();
         
-        launch(args);
+        UiComponent.launchApplication(args);
     }
 }
