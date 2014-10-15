99ef4ee40e4a728e96e4605945c8efcca9a27484
Xiao Jinyu <xjy_333@hotmail.com>
2014-10-15 10:46:43 +0800

    Parser change to singleton

--- src/application/Parser.java
+++ src/application/Parser.java
@@ -6,6 +6,7 @@ package application;
  * @version 2.0
  */
 import org.ocpsoft.prettytime.nlp.PrettyTimeParser;
+import org.ocpsoft.prettytime.shade.org.apache.commons.lang.StringUtils;
 
 import java.util.Date;
 import java.util.List;
@@ -13,68 +14,79 @@ import java.util.List;
 public class Parser {
 
 
-    private Command cmd;
-    private String commandType;
-    private String taskID;
-    private String taskDesc;
-    private Date taskTime;
-
+    private static Command cmd;
+    private static Parser theOne;
     /**
      * This constructs a parser object with an user input 
      * @param userInput   the one line command statement the user inputs
      */
+    Parser(){
 
-    Parser(String userInput){
-        commandType = getFirstWord(userInput).toLowerCase();
-        switch (commandType) {
-        case "undo" : {
-            cmd = new Command(commandType);
-            break;
-        }
-        case "delete" : case "complete" : case "search" : 
-        {
-            taskID =userInput.replace(getFirstWord(userInput), "").trim();
-            cmd = new Command(commandType,taskID);
-            break;
-        }
-        case "add" : {
-            taskDesc = userInput.replace(getFirstWord(userInput), "").trim();
-            List<Date> dates = new PrettyTimeParser().parse(taskDesc);
-            if (dates.size()>0) {
-            taskTime =  new PrettyTimeParser().parse(taskDesc).get(0);
-            }
-            else {
-                taskTime = null;
-            }
-            cmd = new Command(commandType,taskDesc,taskTime);
-            break;
-        }
-        case "edit" : {
-            taskID = userInput.trim().split("\\s+")[1];
-            taskDesc = userInput.replace(getFirstWord(userInput)+" "+taskID,"").trim(); 
-            List<Date> dates = new PrettyTimeParser().parse(taskDesc);
-            if (dates.size()>0) {
-            taskTime =  new PrettyTimeParser().parse(taskDesc).get(0);
-            }
-            else {
-                taskTime = null;
-            }
-            cmd = new Command(commandType, taskID, taskDesc, taskTime);
-            break;	    
-        }
-        default: 
-            break;
-        }
     }
 
-    private String getFirstWord(String input) {
-        String command = input.trim().split("\\s+")[0];
-        return command;
+    public static Parser getInstance() {
+        if (theOne == null) {
+            theOne = new Parser();
+        }
+        return theOne;
     }
+
     /**
+     * return the object of Command class
      * @return the object of Command class 
      */
-    public Command getCmd() {
+    public static Command getCommand(String userInput) {
+        assert ( userInput != null );
+        String commandType = parseCommandType(userInput);
+        String taskID = parseTaskID(userInput);
+        int priority = parsePriority(userInput);
+
         return cmd;
     }
+
+    /**
+     * 
+     * @param input
+     * @return the command type , all letters capitalized
+     */
+    private static String parseCommandType(String input) {
+        String command = input.trim().split("\\s+")[0].toUpperCase();
+        return command;      
+    }
+
+    private static String parseTaskID(String input) {
+        String command = parseCommandType(input); 
+        String taskID = null;
+        if ((command.equalsIgnoreCase("edit")) || (command.equalsIgnoreCase("complete")) || (command.equalsIgnoreCase("delete"))) {
+            assert (input.trim().split("\\s+").length>1);
+            taskID = input.trim().split("\\s+")[1];
+        }
+        return taskID;
+    }
+    private static int parsePriority(String input){
+        int priority = StringUtils.countMatches(input, "!");
+        return priority;
+    }
+
+    public static Date parseTime(String input) {
+        List<Date> dates = new PrettyTimeParser().parse(input);
+        Date taskTime;
+        if (dates.size()>0) {
+            taskTime =  new PrettyTimeParser().parse(input).get(0);
+        }
+        else {
+            taskTime = null;
+        }
+    //    System.out.println(taskTime.toString());
+
+        return taskTime;
+    }
 }
+
+
+
+
+
+
+
+
