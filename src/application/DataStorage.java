988871050f34e286ddb9c16b37c12266ad313895
Kim Hyung Jon <a0115864@nus.edu.sg>
2014-10-22 00:28:12 +0800

    Created JUnit test and made DataStorage able to support it

--- src/application/DataStorage.java
+++ src/application/DataStorage.java
@@ -11,13 +11,12 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.ISODateTimeFormat;
 
 /**
  * 
- * @author A0115864B
+ * @author Kim Hyung Jon (matric number: A0115864B)
  * 
  *
  */
@@ -28,11 +27,27 @@ public class DataStorage {
 	private ArrayList<Task> backup;
 	private DateTimeFormatter fmt = ISODateTimeFormat.dateTime();
 	
+	
+	private static final String STRING_DESC = "Description";
+	private static final String STRING_DATE = "Date";
+	private static final String STRING_END = "End date";
+	private static final String STRING_PRIORITY = "Priority";
+	
+	
+	//@author A0115864B
+	/**
+	 * Constructor.
+	 */
 	public DataStorage() {
 		filename = "Todo.json";
 		initiateFile();
 	}
 	
+	//@author A0115864B
+	/**
+	 * Constructor when filename for external json storage is provided. For unit testing.
+	 * @param name name of the json file for storing the tasks
+	 */
 	public DataStorage(String name) {
 		filename = name;
 		initiateFile();
@@ -40,6 +55,9 @@ public class DataStorage {
 	
 	
 	//@author A0115864B
+	/**
+	 * Confirm that the external json file exists. If not, create it.
+	 */
 	public void initiateFile() {
 		File file = new File(filename);
 		try {
@@ -47,19 +65,24 @@ public class DataStorage {
 				file.createNewFile();
 			}
 		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 	
 	//@author A0115864B
+	/**
+	 * Read json file. Retrieve all tasks and store them in JSONArray.
+	 * @return ArrayList of tasks 
+	 */
 	public ArrayList<Task> retrieveTasks() {
 		tasks.clear();
 		JSONParser parser = new JSONParser();
 		try {
 			tasks = (JSONArray) parser.parse(new FileReader(filename));
 		} catch (IOException e) {
-			System.out.println("IOException");
+			e.printStackTrace();
 		} catch (ParseException e) {
-			System.out.println("ParseException");
+			e.printStackTrace();
 		}
 		System.out.println("File retrieved successfully");
 		System.out.println("Number of tasks retrieved: " + tasks.size());
@@ -68,11 +91,15 @@ public class DataStorage {
 			System.out.println(task.get("Description"));
 		}
 		System.out.println("These are the tasks retrieved\n");
-		return convertJSONArrayToArrayList();
+		return getTasks();
 	}
 	
 	//@author A0115864B
-	public void saveTasks(ArrayList<Task> array) {
+	/**
+	 * Store tasks to external json file
+	 * @param array ArrayList of tasks
+	 */
+	public String saveTasks(ArrayList<Task> array) {
 		convertArrayListToJSONArray(array);
 		try {
 			FileWriter fw = new FileWriter(filename, false);
@@ -80,68 +107,96 @@ public class DataStorage {
 			fw.flush();
 			fw.close();
 		} catch (IOException e) {
-			
+			e.printStackTrace();
 		}
 		System.out.println("One iteration finished\n");
+		return "Saved to" + filename;
 	}
 	
 	//@author A0115864B
-	public ArrayList<Task> convertJSONArrayToArrayList() {
+	/**
+	 * 
+	 * @return
+	 */
+	public ArrayList<Task> getTasks() {
+		return convertJSONArrayToArrayList(tasks);
+	}
+	
+	//@author A0115864B
+	/**
+	 * Converts JSONArray containing the tasks to a form that other components can understand
+	 * @return ArrayList of Tasks objects
+	 */
+	public ArrayList<Task> convertJSONArrayToArrayList(JSONArray array) {
 		ArrayList<Task> list = new ArrayList<Task>();
-		for (int i = 0; i < tasks.size(); i++) {
+		for (int i = 0; i < array.size(); i++) {
 			Task task = new Task();
-			JSONObject obj = (JSONObject) tasks.get(i);
-			task.setDescription((String) obj.get("Description"));
+			JSONObject obj = (JSONObject) array.get(i);
+			task.setDescription((String) obj.get(STRING_DESC));
 			try {
-				if(obj.containsKey("Date")) {
-					String dateString = (String)obj.get("Date");
+				if(obj.containsKey(STRING_DATE)) {
+					String dateString = (String)obj.get(STRING_DATE);
 					DateTime date = fmt.parseDateTime(dateString);
 					task.setDate(date);
 				}
-				if(obj.containsKey("End date")) {
-					String endString = (String)obj.get("End date");
+				if(obj.containsKey(STRING_END)) {
+					String endString = (String)obj.get(STRING_END);
 					DateTime end = fmt.parseDateTime(endString);
 					task.setEndDate(end);
 				}
-			} catch (Exception e) {
+				// Currently priority is not fully supported. May have to update later.
+				if(obj.containsKey(STRING_PRIORITY)) {
+					task.setPriority(((Integer)obj.get(STRING_PRIORITY)));
+				}
 				
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 			list.add(task);
 		}
-		backup = new ArrayList<Task>(list);
+		// Before sending ArrayList of tasks to Controller,
+		// keep a backup of the current state of ArrayLists
+		// so that it can be returned if an undo command is given
 		return list;
 		
 	}
 	
 	//@author A0115864B
 	/**
-	 * 
-	 * @param list
+	 * Converts ArrayList to JSONArray that can be saved to external json file
+	 * @param list ArrayList of Task objects
 	 */
-	public void convertArrayListToJSONArray(ArrayList<Task> list) {
+	public JSONArray convertArrayListToJSONArray(ArrayList<Task> list) {
 		tasks.clear();
 		System.out.println("Converting these tasks to JSON");
 		for (int i = 0; i < list.size(); i++) {
 			JSONObject obj = new JSONObject();
-			obj.put("Description", list.get(i).getDescription());
+			obj.put(STRING_DESC, list.get(i).getDescription());
 			System.out.println(list.get(i).getDescription());
+			obj.put(STRING_PRIORITY, list.get(i).getPriority());
 			try {
 				if (list.get(i).getDate() != null) {
 					String date = fmt.print(list.get(i).getDate());
-					obj.put("Date", date);
+					obj.put(STRING_DATE, date);
 				}
 				if (list.get(i).getEndDate() != null) {
 					String end = fmt.print(list.get(i).getEndDate());
-					obj.put("End date", end);
+					obj.put(STRING_END, end);
 				}
 			} catch (Exception e) {
-				
+				e.printStackTrace();
 			}
 			tasks.add(obj);
 		}
+		return tasks;
 	}
 	
 	//@author A0115864B
+	/**
+	 * Support for undo command
+	 * @return backup ArrayList of tasks that was saved before last operation
+	 * (Currently not implemented)
+	 */
 	public ArrayList<Task> getPastVersion() {
 		return backup;
 	}
