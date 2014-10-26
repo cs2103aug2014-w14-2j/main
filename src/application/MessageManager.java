73743eee16ce01f22bf7c50b721c94822fd89cfc
Kim Hyung Jon <a0115864@nus.edu.sg>
2014-10-26 19:21:46 +0800

    Some more work done on MM and TM

--- src/application/MessageManager.java
+++ src/application/MessageManager.java
@@ -2,15 +2,15 @@ package application;
 
 public class MessageManager {
     
-    private final String NOTIFY_ADDED = "%s added as Task %s";
-    private final String NOTIFY_DELETED = "%s deleted";
-    private final String NOTIFY_EDITED = "%s edited successfully";
-    private final String NOTIFY_COMPLETED = "%s finished";
+    private final String NOTIFY_ADDED = "added as Task";
+    private final String NOTIFY_DELETED = "deleted";
+    private final String NOTIFY_EDITED = "edited successfully";
+    private final String NOTIFY_COMPLETED = "finished";
     private final String NOTIFY_UNDO = "Change unmade";
     
     private final String WARNING_INVALID_COMMAND = "Cannot understand this command!";
     private final String WARNING_INVALID_SYNTAX = "Cannot add that task";
-    private final String WARNING_INVALID_ID = "Cannot find that task!";
+    private final String WARNING_INVALID_ID = "Cannot find";
     private final String WARNING_INVALID_DATE = "Cannot understand when %s is";
     private final String WARNING_INVALID_DATES = "Cannot understand when %s and %s are";
 
