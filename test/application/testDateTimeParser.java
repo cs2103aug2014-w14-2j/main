1062c6fce68e7ae36e3adcf9fae4ee16cb23b6cf
Xiao Jinyu <xjy_333@hotmail.com>
2014-10-29 10:58:38 +0800

    no message

--- test/application/testDateTimeParser.java
+++ test/application/testDateTimeParser.java
@@ -4,11 +4,17 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.NoSuchMethodException;
+
+import com.joestelmach.natty.Parser;
+
 public class testDateTimeParser {
+    Parser test = new Parser();
 
     @Test
     public void test() {
         fail("Not yet implemented");
     }
-
 }
