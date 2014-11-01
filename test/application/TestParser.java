cb25e0483e34f539f8df32beb957b9e688ed34a9
Xiao Jinyu <xjy_333@hotmail.com>
2014-11-01 22:55:14 +0800

    parser.getCommandInfo() throws an exception when invalid user input is entered

--- test/application/TestParser.java
+++ test/application/TestParser.java
@@ -9,34 +9,32 @@ import org.joda.time.DateTime;
 public class TestParser {
     public static void main(String[] args) {
     Parser parser = new Parser();
+    try {
+    CommandInfo cmd = parser.getCommandInfo("delete [] next friday noon ");
+    System.out.println(cmd.getMessage());
+    
+    //  ArrayList<String> IDs = new ArrayList<String>();
+    //  IDs = cmd.getTaskIDs();
+    //  for (int i = 0; i<IDs.size();i++) {
+    //      System.out.println(IDs.get(i));
+    //  }
+      
+      System.out.println("task desc is " +cmd.getTaskDesc());
+      System.out.println("boolean"+ cmd.isCompleted());
+    //  System.out.println(cmd.getPriority());
+      System.out.println("start time is " + cmd.getStartDateTime());
+      System.out.println("end time is " + cmd.getEndDateTime());
+      System.out.println("keyword is "+cmd.getTaskDesc());
 
-    CommandInfo cmd = parser.getCommandInfo("add []");
 
   //  System.out.println(cmd.getCommandType());
 
-    try { 
-    cmd.validateUserInput();
     }
     catch (MismatchedCommandException e) {
-        System.out.println("21");
         System.out.println(e);
     }
     
-    System.out.println(cmd.getMessage());
-    
-  //  ArrayList<String> IDs = new ArrayList<String>();
-  //  IDs = cmd.getTaskIDs();
-  //  for (int i = 0; i<IDs.size();i++) {
-  //      System.out.println(IDs.get(i));
-  //  }
-    
-    System.out.println("task desc is " +cmd.getTaskDesc());
-    System.out.println("boolean"+ cmd.isCompleted());
-  //  System.out.println(cmd.getPriority());
-    System.out.println("start time is " + cmd.getStartDateTime());
-    System.out.println("end time is " + cmd.getEndDateTime());
-    System.out.println("keyword is "+cmd.getTaskDesc());
-
+   
  
 }
 }
