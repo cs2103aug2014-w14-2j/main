801443434cc60e280e133a043448135489e68e32
Kim Hyung Jon <a0115864@nus.edu.sg>
2014-11-10 21:37:56 +0800

    comments

--- src/UI/UITooltips.java
+++ src/UI/UITooltips.java
@@ -3,6 +3,12 @@ package UI;
 import java.util.Random;
 import java.util.ArrayList;
 
+//@author A0115864B
+/**
+ * Stores and returns random tooltips
+ * @author Kim Hyung Jon
+ *
+ */
 public class UITooltips {
     
     Random rand = new Random();
